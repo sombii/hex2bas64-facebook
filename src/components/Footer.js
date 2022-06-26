3744ad22292d2b234bd07b268ef89e93f0ae4a62
sombii <blackknightstd@gmail.com>
2022-06-26 14:33:29 +0545

    fixed a typo in footer credit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="mt-32 p-8 border-t border-gray-700">
             <div className="max-w-5xl mx-auto flex justify-center items-center flex-col">
-                <p>Created by <a href="https://sanjogr.com.cp" className="border-b-2 border-blue-400" target="_blank" rel={"noreferrer noopener"}>sanjogr.com.np</a> (sombex.com) | {new Date().getFullYear()}</p>
+                <p>Created by <a href="https://sanjogr.com.np" className="border-b-2 border-blue-400" target="_blank" rel={"noreferrer noopener"}>sanjogr.com.np</a> (sombex.com) | {new Date().getFullYear()}</p>
             </div>
         </footer>
     )
